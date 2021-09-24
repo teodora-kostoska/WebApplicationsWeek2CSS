009243d79173d6a8cd3a54719874c0edfd6dc07f
teodora-kostoska <45472517+teodora-kostoska@users.noreply.github.com>
2021-09-24 12:33:50 +0300

    Changes to CSS and Javascript

--- src/index.js
+++ src/index.js
@@ -8,18 +8,6 @@ document
 function addComment() {
   var text = document.getElementById("textbox").value;
   var rating = document.getElementById("rating").value;
-  var ratingvalue = "";
-  for (var i = 0; rating > i; i++) {
-    if (rating == 1) {
-      ratingvalue = "*";
-    } else if (rating == 2) {
-      ratingvalue = "* *";
-    } else if (rating == 3) {
-      ratingvalue = "* * *";
-    } else {
-      ratingvalue = "* * * *";
-    }
-  }
   var ul = document.getElementById("unordered");
   var div2 = document.createElement("div");
   div2.className = "comment";
@@ -29,12 +17,12 @@ function addComment() {
   divtext.className = "comment-text";
   var button = document.createElement("button");
   button.innerText = "Remove comment";
-  button.className = "element-removal";
+  button.className = "remove-comment";
   button.style.visibility = "hidden";
   button.onclick = function () {
     ul.removeChild(div2);
   };
-  divrating.appendChild(document.createTextNode(ratingvalue));
+  divrating.appendChild(document.createTextNode(rating));
   divtext.appendChild(document.createTextNode(text));
   div2.appendChild(divrating);
   div2.appendChild(divtext);
@@ -42,7 +30,7 @@ function addComment() {
   ul.appendChild(div2);
 }
 function removeComments() {
-  var button = document.getElementsByClassName("element-removal");
+  var button = document.getElementsByClassName("remove-comment");
   var buttonlength = button.length;
   for (var i = 0; i < buttonlength; i++) {
     button[i].style.visibility = "visible";
